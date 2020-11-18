181d95ac5ea8c2828440c76017f0393c8aed9cbf
flashdhruv <73646382+flashdhruv@users.noreply.github.com>
2020-11-19 01:05:14 +0400

    Update listprod.js
    added the sql queries.
    1) if name is not null it'll return products with matching names
    2) if name is null then it returns the full dataset 
    3) Set up the table that prints the data out.
    4) set up the link to addcart however i'm not sure about it

--- 304_lab7_starter_node/routes/listprod.js
+++ 304_lab7_starter_node/routes/listprod.js
@@ -11,9 +11,58 @@ router.get('/', function(req, res, next) {
     
     /** $name now contains the search string the user entered
      Use it to build a query and print out the results. **/
-
+    let query1 = "SELECT productName, quantity, price FROM product JOIN orderProduct ON product.productId = orderproduct.productId WHERE productName LIKE %(%s)% "
+    let query2 = "SELECT productName, quantity, price FROM product JOIN orderProduct ON product.productId = orderproduct.productId";
+    
+    
     /** Create and validate connection **/
-
+(async function() {
+        try {
+            let pool = await sql.connect(dbConfig);
+            
+            if(name == null){
+             let results = await pool.request()
+             .query(query2);
+                
+                res.write("<table>" +
+                "<tr>" +
+                    "<th>Name</th>" +
+                    "<th>Quantity</th>" +
+                    "<th>Price</th>" +
+                     "<th>Add to Cart</th>" +
+                "</tr>");
+                
+                for(let i = 0; i<results.recordset.length; i++) {
+                    let result = results.recordset[i];
+                    res.write("<tr>" +
+                    "<td>" + result.productName + "</td>" +
+                    "<td>" + result.quantity + "</td>" +
+                    "<td>" + result.price + "</td>" +
+                    "<td>" + addcart?id=<result.productid>&name=<result.productName>&price=<result.price> + "</td>" +
+                    
+                
+            }
+            else{
+            let results = await pool.request()
+                .query(query1, (name));
+                
+                res.write("<table>" +
+                "<tr>" +
+                    "<th>Name</th>" +
+                    "<th>Quantity</th>" +
+                    "<th>Price</th>" +
+                "</tr>");
+                              
+                     for(let i = 0; i<results.recordset.length; i++) {
+                    let result = results.recordset[i];
+                    res.write("<tr>" +
+                    "<td>" + result.productName + "</td>" +
+                    "<td>" + result.quantity + "</td>" +
+                    "<td>" + result.price + "</td>" +
+                    "<td>" + addcart?id=<result.productid>&name=<result.productName>&price=<result.price> + "</td>" +
+            }
+                              
+                              res.write("</table>");
     /** Print out the ResultSet **/
 
     /** 
@@ -26,7 +75,10 @@ router.get('/', function(req, res, next) {
         let num = 2.89999;
         num = num.toFixed(2);
     **/
-
+        catch (err) {
+            console.dir(err);
+            res.write(err);
+        }
     res.end();
 });
 
